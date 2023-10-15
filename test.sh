140b5221b96ab6333c34941a6ca3e0aa47d6cf1d
Andreas Herrmann <andreas.herrmann@tweag.io>
2023-10-15 18:00:10 +0200

    add instructions

---
+++ test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "$(dirname -- "${BASH_SOURCE[0]}")"
+
+set -x
+
+# Build the Bazel remote worker.
+(
+  cd ../00-bazel-worker
+  bazel build //src/tools/remote:worker
+)
+
+# Run the Bazel remote worker in the background.
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+(
+  cd ../00-bazel-worker
+  mkdir -p tmp/work tmp/cas
+  bazel run //src/tools/remote:worker -- \
+    --listen_port=7070 \
+    --work_path="$PWD/tmp/work" \
+    --cas_path="$PWD/tmp/cas" \
+    --sandboxing \
+    --sandboxing_writable_path=/run/shm \
+    --sandboxing_tmpfs_dir=/tmp \
+    --sandboxing_block_network
+) &
+
+sleep 2
+
+bazel clean
+bazel build hello
+# Should report remote processes, e.g.
+#
+#   INFO: 8 processes: 5 internal, 3 remote.
